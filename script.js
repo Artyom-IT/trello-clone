11192f0aad219c68619092f24a21d6c478fe657b
Артём Марченко <artemmarcenko@MacBook-Air-Artem.local>
2020-07-12 21:18:50 +0300

    Added delete functional

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 document.querySelectorAll(".note").forEach(Note.draggingHandle);
 document.querySelectorAll(".column").forEach(Note.btnHandler);
 
+document.querySelectorAll('.close').forEach(el => el.addEventListener('click', () => {
+  el.closest('.note').remove()
+}))
+
+document.querySelectorAll('.close-column').forEach(el => el.addEventListener('click', () => {
+  el.parentElement.remove()
+}))
+
 document
   .querySelector("[data-action-addColumn]")
   .addEventListener("click", Column.add);
